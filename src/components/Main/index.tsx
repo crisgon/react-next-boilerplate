bbc1f5f4a90acd19fe7f2010d443e893296ab001
Cristiano <kristiano.gon@gmail.com>
2021-02-19 01:31:00 -0300

    chore: add styled components and jest-styled-components

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
+import * as S from './styles';
+
 export function Main() {
   return (
-    <main>
-      <h1>React Avançado</h1>
-    </main>
+    <S.Wrapper>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado"
+      />
+      <S.Title>React Avançado</S.Title>
+      <S.Description>
+        Typescript, ReactJS, NextJS e Styled Components
+      </S.Description>
+
+      <S.Ilustration
+        src="/img/hero-illustration.svg"
+        alt="Rapaz sentado em frente a tela com códigos"
+      />
+    </S.Wrapper>
   );
 }
