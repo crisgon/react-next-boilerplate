c27989a7e7bba2083832621b64005bb2503e89ee
Cristiano <kristiano.gon@gmail.com>
2021-02-21 22:06:49 -0300

    chore: add next-pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,9 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A project using Typescript, NextJS and Styled Components"
